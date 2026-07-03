252615fe091e93bd5348d31fff101227416f187f
sstep132 <sstep132@students.kennesaw.edu>
2026-07-03 10:08:18 -0400

    Add function to toggle navigation menu visibility
    Implement function for responsive navigation menu.

---
+++ script.js
@@ -0,0 +1,9 @@
+/*To make navigation menu work on small screens*/
+function myFunction() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
